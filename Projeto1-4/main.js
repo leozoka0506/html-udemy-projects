1b9f6558cbcd781bfa48df74293fd7a6b63f8280
leozoka0506 <leowojcik04@gmail.com>
2025-04-01 11:13:44 -0300

    conteudo do projeto 1 ao projeto 4

---
+++ Projeto1-4/main.js
@@ -0,0 +1,10 @@
+// let firstName = prompt('Digite o seu primeiro nome:')
+// let lastName = prompt('Digite o seu sobrenome:')
+// let age = parseInt(prompt('Digite a sua idade:'))
+
+// age = age + 2
+
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(age)
