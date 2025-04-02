f336d8d453201710a5b79058461387866b1303df
leozoka0506 <leowojcik04@gmail.com>
2025-04-02 10:48:50 -0300

    conteudo do projeto 8, if e else

---
+++ Projeto8/main.js
@@ -0,0 +1,21 @@
+let nota = parseInt(prompt('Qual a nota?'))
+
+if (nota>100){
+    alert('NOTA INVALIDA')
+}
+else if (nota>89){
+    alert('NOTA A')
+}
+else if(nota>79){
+    alert('NOTA B')
+}
+else if(nota>69){
+    alert('NOTA C')
+}
+else if(nota>59){
+    alert('NOTA D')
+}
+else{
+    alert('NOTA F')
+}
+
