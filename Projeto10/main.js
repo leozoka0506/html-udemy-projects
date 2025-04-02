e97e69f2774be6599046a0010c6e3bf7707267b8
leozoka0506 <leowojcik04@gmail.com>
2025-04-02 13:23:55 -0300

    conteudo do projeto 10, Calculo de gorjeta

---
+++ Projeto10/main.js
@@ -0,0 +1,4 @@
+let conta = parseFloat(prompt('Digite o valor da conta: '))
+let gorjeta = parseFloat(prompt('Digite o valor percentual da gorjeta: '))
+
+alert('O valor total é '+ ((gorjeta/100)+1)*conta+ 'R$')
